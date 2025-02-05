1c9847ef8f844ba13276fd95d1e33befc57839fa
Robin Dittmar <robindittmar@gmail.com>
2025-02-05 07:02:07 +0100

    fixed typeorm datasource

--- src/core/database.ts
+++ src/core/database.ts
@@ -4,12 +4,13 @@ import { CategoryModel } from "../models/category-model.ts";
 export const AppDataSource: DataSource = new DataSource({
   type: "postgres",
   host: Deno.env.get("DENO_ENV") === "development" ? "localhost" : "mysql",
-  port: 3306,
+  port: 5432,
   maxQueryExecutionTime: 5000,
   poolSize: 10,
   username: "reatreatbot_user",
   password: "6sBfdAq$HkgNA$MwkmwaXQ6FH&c3rpd2",
-  database: "retreatbot",
+  database: "postgres",
+  schema: "retreatbot",
   logging: Deno.env.get("DENO_ENV") === "development",
   entities: [
     CategoryModel,
